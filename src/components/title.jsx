0fa746301154a5d26ac9e0f1a72bf0c1a9957407
alon <alontiti9@gmail.com>
2025-11-21 21:31:43 +0200

    teacher page

--- src/components/title.jsx
+++ src/components/title.jsx
@@ -1,11 +1,18 @@
 import LineSvg from "../assets/line.svg?react";
 import "./title.css";
 
-export default function Title({ text }) {
+export default function Title({ text, subtitle = null }) {
   return (
     <div className="title-wrapper">
       <h2 className="lined-title">{text}</h2>
       <LineSvg className="title-line" />
+      {subtitle == null ? (
+        <></>
+      ) : (
+        <>
+          <h4>{subtitle}</h4>
+        </>
+      )}
     </div>
   );
 }
--- src/pages/Teacher.jsx
+++ src/pages/Teacher.jsx
@@ -1,3 +1,59 @@
+import Title from "../components/title";
+import "./Teacher.css";
+import teacherPhoto from "../assets/pictures/teacher.jpg";
+import dan1Photo from "../assets/pictures/dan1.svg";
+import dan2Photo from "../assets/pictures/dan2.svg";
+
 export default function Teacher() {
-  return <h1>Welcome to the Home Page</h1>;
+  return (
+    <div className="teacher-page">
+      <Title text="אפי דינר" subtitle="מייסד ומורה בית הספר"></Title>
+
+      <img src={teacherPhoto} alt="Teacher" className="teacher-photo" />
+
+      <div className="trophies-wrapper">
+        {/* absolute overlay */}
+
+        <div className="trophies-row">
+          <div id="teacher-transition" className="top-transition"></div>
+          <div id="bamboo-left" className="top-transition bamboo"></div>
+          <div id="bamboo-right" className="top-transition bamboo"></div>
+
+          <div className="trophy">
+            <img src={dan1Photo} alt="" />
+            <div className="trophy-text">דרגת דאן 1 בקראטה</div>
+          </div>
+
+          <div className="trophy">
+            <img src="/path/to/trophy2.png" alt="" />
+            <div className="trophy-text">תיאור 2</div>
+          </div>
+
+          <div className="trophy">
+            <img src={dan2Photo} alt="" />
+            <div className="trophy-text">דרגת דאן 2 באייקידו</div>
+          </div>
+        </div>
+      </div>
+
+      <div className="mycard padded">
+        <div className="card-info">
+          <p>
+            שמי אפי.<br></br>
+            אני לומד ומתאמן מאז 1981.
+          </p>
+          <h5>שירות צבאי:</h5>
+          <p>
+            שירתתי כמג״יסט בגדוד 13 בחטיבת גולני. בחלק ניכר מהשירות שירתתי
+            בלבנון, בעיר צידון ובמארבים על נהר האלמלי. לאחר שהתפנינו לרצועת
+            הביטחון, מאסתי בלחימה, ולאחר שלושה חודשים בכלא צבאי המשכתי שירות לא
+            משמעותי עד תום הזמן הכולל.
+          </p>
+        </div>
+        <div className="card-graphic padded" id="taichiGraphic">
+          <img class="image" alt="Taichi" />
+        </div>
+      </div>
+    </div>
+  );
 }
