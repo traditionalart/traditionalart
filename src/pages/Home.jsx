659f7887198c6cbd266d6bb569cd0f1ac79d0a08
alon <alontiti9@gmail.com>
2025-12-09 00:00:33 +0200

    work on colors palette and home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "./Home.css";
+import OpeningVideo from "../assets/graphics/home/opener.webm"; // adjust path
+
 import Collapsible from "../components/collapsible";
 import { EffectFade, Navigation, Pagination, Autoplay } from "swiper/modules";
 import Title from "../components/title";
+import Logo from "../assets/graphics/home/Banner.svg";
 
 // Import Swiper styles
 import "swiper/css";
@@ -16,7 +19,25 @@ export default function Home() {
   //const images = ["./slideshow/img1", "./slideshow/img2", "./slideshow/img3"]ddfdfd;
 
   return (
-    <div className="page">
+    <div id="home-page" className="page">
+      <div className="fs-video-container">
+        <video
+          className="fs-video-container__video"
+          autoPlay
+          loop
+          muted
+          playsInline
+          src={OpeningVideo}
+        />
+        <div className="fs-video-container__overlay" />
+        <div className="fs-video-container__logo-wrapper">
+          <img src={Logo} alt="Logo" className="fs-video-container__logo" />
+          <div className="fs-video-container__subtitle">
+            בית הספר לאומנויות לחימה
+          </div>
+        </div>
+      </div>
+
       <div className="mycard padded">
         <header className="card-info centered">נאנקינג טאי' צי'</header>
 
